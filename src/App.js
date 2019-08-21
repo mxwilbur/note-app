accc1cf7b50b10e6f903ef0f2c2a5513403803f7
mxwilbur <mxwilbur@outlook.com>
2019-08-21 13:50:54 +0000

    added axios app for loading content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
 
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -17,13 +18,20 @@ class App extends React.Component {
       showNote: ! this.state.showNote
     });
   }
+
+  getNotes = () => {
+    axios.get('https://firehose-note-api.herokuapp.com/notes')
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render() {
     const { showNote } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote ? <Note /> : <List /> }
+        { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
       </div>
     );
   }
