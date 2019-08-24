dd745f80504acf74ff68f6df1eff0b358a91a3c5
mxwilbur <mxwilbur@outlook.com>
2019-08-24 18:39:29 +0000

    add a new tag form

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,5 +1,12 @@
 import React from 'react';
 class NoteCard extends React.Component {
+  renderTags(note) {
+    return note.tags.map((tag, index) =>
+      <span className="note-card-tag" key={index}>
+        {tag.name}
+      </span>
+    );
+  }
   render() {
     const { note, getNote, deleteNote } = this.props;
     return (
@@ -9,6 +16,9 @@ class NoteCard extends React.Component {
         </div>
         <div className="note-card-content">
           {note.content}
+        </div>
+        <div className="note-card-tags">
+          {this.renderTags(note)}
         </div>
         <span className="note-card-delete" onClick={() => deleteNote(note.id)}>
           <i className="material-icons">close</i>
