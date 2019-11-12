08f734350311a10aa7caacf65aba50cbedddebf8
mxwilbur <mxwilbur@outlook.com>
2019-11-12 22:06:33 +0000

    add gif

--- src/components/Note.js
+++ src/components/Note.js
@@ -93,7 +93,7 @@ class Note extends React.Component {
         </form>
         <div className="tag-container">
           <div className="tag-button-container">
-            {renderTagForm()}
+            {this.renderTagForm(note)}
           </div>
           <div className="tag-list-container">
             {this.renderTags(note)}
